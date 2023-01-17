a7fba189aaf0b31498e4da2cf4030d2b5b5e6f45
Alex <best1972@list.ru>
2023-01-17 12:05:21 +0300

    made task 2

--- Program.cs
+++ Program.cs
@@ -22,3 +22,23 @@ int[] GetArray(int size, int minValue, int maxValue)
     }
     return res;
 }*/
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму 
+//элементов, стоящих на нечётных позициях. [3, 7, 23, 12] -> 19  [-4, -6, 89, 6] -> 0
+/*int[] array = GetArray(7, 0, 100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int sum = 0;
+for(int i = 1; i < array.Length; i=i+2)
+{
+    sum = sum + array[i];
+}
+Console.Write($"Сумма элементов стоящих на нечётных позициях: {sum}");
+//Метод--------------------------------------------------------
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}*/
